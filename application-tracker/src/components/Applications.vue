ff3f426f3d2e67c9ba86a62b36fbad5aec83719f
907riley <907riley@gmail.com>
2023-07-25 22:02:26 -0700

    typescript is angry at my objects, this was supposed to SAVE me time

--- application-tracker/src/components/Applications.vue
+++ application-tracker/src/components/Applications.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { ref } from 'vue'
+    import { ref, type App, type Ref} from 'vue'
 
     interface Application {
         title: string;
@@ -43,17 +43,29 @@
         },  
     ]
 
-    const exampleApplications = ref(exampleApplicationsJSON)
+    const exampleApplications: Ref<Application[]> = ref(exampleApplicationsJSON)
     const applicationFields = ref(applicationFieldsArray)
+    const searchBar = ref('')
 
-    console.log(exampleApplicationsJSON)
+    function filteredApplications() {
+        return exampleApplications.value.filter((app) => {
+            let property:keyof Application
+                for (const property in app) {
+                    if (typeof property === 'string') {
+                        app[property].toLowerCase()
+                    }
+                }
+            }
+        )
+    }
+
 </script>
 
 <template>
     <div class="h-fit p-5 flex-1 flex flex-col">
         <div class="search-add-bar-wrapper h-32 flex flex-row">
             <div class="search-bar-wrapper my-auto">
-                <input type="text" placeholder="Search" class="search-bar text-4xl border-2 py-2 px-4 border-black rounded-full font-genos font-black"/>
+                <input v-model="searchBar" type="text" placeholder="Search" class="search-bar text-4xl border-2 py-2 px-4 border-black rounded-full font-genos font-black"/>
             </div>
             <div class="flex-1"></div>
             <div class="add-application-button-wrapper border-2 border-black w-fit my-auto">
@@ -65,7 +77,7 @@
                 <button v-for="fields in applicationFields" class="col-span-1 py-4">{{ fields }}</button>
             </div>
             <div class="information-wrapper flex flex-col flex-1">
-                <div v-for="applications in exampleApplications" class="grid grid-cols-8 bg-white font-genos">
+                <div v-for="applications in filteredApplications()" class="grid grid-cols-8 bg-white font-genos">
                     <div class="col-span-1 text-2xl p-2 border-r border-b border-black flex"> 
                         <div class="m-auto flex-1 text-center">
                             {{ applications.title }}
@@ -88,7 +100,7 @@
                     </div>
                     <div class="col-span-1 text-2xl p-2 border-r border-b border-black flex"> 
                         <div class="m-auto flex-1 text-center">
-                            {{ applications.date_applied.getDate() }}
+                            {{ applications.date_applied.getMonth() }} / {{ applications.date_applied.getDate() }} / {{ applications.date_applied.getFullYear() }}
                         </div>
                     </div>
                     <div class="col-span-1 text-2xl p-2 border-r border-b border-black flex"> 
@@ -98,7 +110,11 @@
                     </div>
                     <div class="col-span-1 text-2xl p-2 border-r border-b border-black flex"> 
                         <div class="m-auto flex-1 text-center">
-                            <!-- <a href={{ applications.link }}> link</a> -->
+                            <a target="_blank" rel="noopener noreferrer" v-if="applications.link.toLowerCase() === 'wayup'"> WayUp </a>
+                            <a target="_blank" rel="noopener noreferrer" v-else-if="applications.link.toLowerCase() === 'linkedin'"> Linkedin </a>
+                            <a target="_blank" rel="noopener noreferrer" v-else :href=applications.link class="m-auto">
+                                <span class="material-symbols-outlined text-3xl">open_in_browser</span>
+                            </a>
                         </div>
                     </div>
                 </div>
