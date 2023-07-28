82ea1075f147c12b13aeadfb052f38c3fd23a5b1
907riley <907riley@gmail.com>
2023-07-28 11:30:58 -0700

    validatin form stuff now for new job hunt

--- application-tracker/src/components/ApplicationTracker.vue
+++ application-tracker/src/components/ApplicationTracker.vue
@@ -7,10 +7,27 @@
     import { ref } from 'vue';
 
     const addingHunt = ref(false)
+    const jobTypes = ref([
+        "Internship",
+        "Junior",
+        "Mid",
+        "Senior"
+    ])
+
+    const huntTitle = ref('')
+    const goalSalary = ref('')
+    const goalJobType = ref('')
+    const goalLocation = ref('')
+    const goalTechStack = ref('')
+    const goalJobTitle = ref('')
 
     function displayHuntForm() {
         console.log("displaying hunt form")
         addingHunt.value = true
+    }
+
+    function submitJobHunt() {
+        console.log(huntTitle, goalSalary, goalJobType, goalLocation, goalTechStack, goalJobTitle)
     }
 </script>
 
@@ -20,46 +37,49 @@
             <div class="title-wrapper text-white font-bold text-5xl flex justify-center p-5 border-b-2 border-black">
                 <span>New Job Hunt</span>
             </div>
-            <div class="form-content-wrapper flex flex-col gap-4 m-5">
-                <div class="title-form-wrapper gap-4 flex flex-col text-3xl">
+            <div class="form-content-wrapper flex flex-col gap-8 m-10">
+                <div class="title-form-wrapper gap-6 flex flex-col text-3xl">
                     <div class="flex flex-row bg-white gap-4">
                         <label for="hunt_title" class="place-self-start"> Hunt Title: </label>
-                        <div class="flex-1 bg-red-200"></div>
-                        <input id="hunt_title" type="text" class="bg-zinc-300 border border-black place-self-end"/>
+                        <div class="flex-1 "></div>
+                        <input id="hunt_title" type="text" v-model="huntTitle" class="p-1 px-2 bg-zinc-300 border border-black place-self-end"/>
                     </div>
                 </div>
                 <div class="job-goal-wrapper border-b-2 border-black flex justify-center text-2xl font-bold">
-                    <span>Job Goals</span>
+                    <span>Job Goals (optional)</span>
                 </div>
-                <div class="information-wrapper gap-4 flex flex-col text-3xl">
+                <div class="information-wrapper gap-6 flex flex-col text-3xl">
                     <div class="flex flex-row bg-white gap-4">
                         <label for="goal_salary" class="place-self-start"> Salary: </label>
-                        <div class="flex-1 bg-red-200"></div>
-                        <input id="goal_salary" type="text" class="bg-zinc-300 border border-black place-self-end"/>
+                        <div class="flex-1 "></div>
+                        <input id="goal_salary" type="number" v-model="goalSalary" class="p-1 px-2 bg-zinc-300 border border-black place-self-end"/>
                     </div>
                     <div class="flex flex-row bg-white gap-4">
                         <label for="goal_job_type" class="place-self-start"> Job Type: </label>
-                        <div class="flex-1 bg-red-200"></div>
-                        <input id="goal_job_type" type="text" class="bg-zinc-300 border border-black place-self-end"/>
+                        <div class="flex-1 "></div>
+                        <!-- <input id="goal_job_type" type="text" class="p-1 px-2 bg-zinc-300 border border-black place-self-end"/> -->
+                        <select v-model="goalJobType" class="p-1 px-2 bg-zinc-300 border border-black place-self-end">
+                            <option v-for="job in jobTypes" :value="job.value">{{job}}</option>
+                        </select>
                     </div>
                     <div class="flex flex-row bg-white gap-4">
                         <label for="goal_location" class="place-self-start"> Location: </label>
-                        <div class="flex-1 bg-red-200"></div>
-                        <input id="goal_location" type="text" class="bg-zinc-300 border border-black place-self-end"/>
+                        <div class="flex-1 "></div>
+                        <input id="goal_location" type="text" v-model="goalLocation" class="p-1 px-2 bg-zinc-300 border border-black place-self-end"/>
                     </div>
                     <div class="flex flex-row bg-white gap-4">
                         <label for="goal_tech_stack" class="place-self-start"> Tech Stack: </label>
-                        <div class="flex-1 bg-red-200"></div>
-                        <input id="goal_tech_stack" type="text" class="bg-zinc-300 border border-black place-self-end"/>
+                        <div class="flex-1 "></div>
+                        <input id="goal_tech_stack" type="text" v-model="goalTechStack" class="p-1 px-2 bg-zinc-300 border border-black place-self-end"/>
                     </div>
                     <div class="flex flex-row bg-white gap-4">
                         <label for="goal_job_title" class="place-self-start"> Job Title: </label>
-                        <div class="flex-1 bg-red-200"></div>
-                        <input id="goal_job_title" type="text" class="bg-zinc-300 border border-black place-self-end"/>
+                        <div class="flex-1 "></div>
+                        <input id="goal_job_title" type="text" v-model="goalJobTitle" class="p-1 px-2 bg-zinc-300 border border-black place-self-end"/>
                     </div>
                 </div>
                 <div class="button-wrapper flex border-2 border-black">
-                    <button id="done-button" class="flex-1 p-2 font-bold text-3xl">Done</button>
+                    <button @click="submitJobHunt" id="done-button" class="flex-1 p-2 font-bold text-3xl">Done</button>
                 </div>
             </div>
         </div>
