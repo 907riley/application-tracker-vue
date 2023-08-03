69043ca408e26fab54efc5b7f637185aa9f69c87
Riley Sikes <907riley@gmail.com>
2023-08-03 08:27:35 -0700

    little typo in supabase env

--- application-tracker/src/clients/supabase.ts
+++ application-tracker/src/clients/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 import { type Database } from '../db_types/supabase'
 
-const supabaseUrl = import.meta.env.VITE_SUAPURL
+const supabaseUrl = import.meta.env.VITE_SUPAURL
 const supabaseKey = import.meta.env.VITE_SUPAKEY
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
